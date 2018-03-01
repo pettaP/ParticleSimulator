83f531138349389f82dd464afb7d122f47823ec8
hfurhoff <hfurhoff@kth.se>
2018-03-01 10:26:16 +0100

    clionproject

---
+++ cmake-build-debug/CMakeFiles/fuckclion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuckclion.dir/serialordon.cpp.o"
+  "CMakeFiles/fuckclion.dir/commonordon.cpp.o"
+  "fuckclion.pdb"
+  "fuckclion.exe"
+  "libfuckclion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fuckclion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
